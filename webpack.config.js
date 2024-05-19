3909c702b910935586a05a32a46be3af00185453
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2024-05-19 19:56:34 +0300

    add sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,34 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: './src/index.js',
+    entry: ['./src/index.js',
+            './src/styles/main.scss',
+    ],
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
-    plugins: [new HtmlWebpackPlugin({
-        template: './src/index.html',})],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader",
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: "styles/main.css",
+        }),
+    ],
+    mode: 'production',
 };
