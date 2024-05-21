3f8eae339a1574a2e1c0486a5d5f731713db4f21
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2024-05-21 21:11:30 +0300

    add file-loader & html-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const loader = require('sass-loader');
 
 module.exports = {
     entry: ['./src/index.js',
@@ -19,6 +20,17 @@ module.exports = {
                     "css-loader",
                     "sass-loader",
                 ],
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.(png|jpe?g|gif|mp4)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[name][ext]',
+                },
             },
         ],
     },
